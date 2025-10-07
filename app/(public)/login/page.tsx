d37bbae31788b50b81885993779ce853f77f501b
May Sunktong <pattaranan.sunktong@gmail.com>
2025-10-07 15:16:16 +0200

    put a new layout from Shadcn

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -1,3 +1,5 @@
+import { GalleryVerticalEnd, Spool } from "lucide-react";
+import { LoginForm } from "../../../components/login-form";
 import LogInForm from "./LogInForm";
 
 export default function LogInPage() {
@@ -5,6 +7,30 @@ export default function LogInPage() {
     <div>
       Log In page
       <LogInForm />
+      <div className="grid min-h-svh lg:grid-cols-2">
+        <div className="bg-muted relative hidden lg:block">
+          <img
+            src="/placeholder.svg"
+            alt="Image"
+            className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          />
+        </div>
+        <div className="flex flex-col gap-4 p-6 md:p-10">
+          <div className="flex justify-center gap-2 md:justify-start">
+            <a href="#" className="flex items-center gap-2 font-medium">
+              <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+                <Spool className="size-4" />
+              </div>
+              Huddle
+            </a>
+          </div>
+          <div className="flex flex-1 items-center justify-center">
+            <div className="w-full max-w-xs">
+              <LoginForm />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
