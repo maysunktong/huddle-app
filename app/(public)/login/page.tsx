468e4d6b7bf7fa0098e15d895bbccf30fbd86bd0
May Sunktong <pattaranan.sunktong@gmail.com>
2025-10-10 14:09:27 +0200

    Fix copyright

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -14,7 +14,7 @@ export default function LogInPage() {
           <div className="block lg:hidden">
             <DiscoBall />
           </div>
-          <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-col flex-1 justify-center items-center">
             <div className="w-full max-w-xs text-center">
               <div className="flex justify-center items-center gap-2 py-8">
                 <Logo />
@@ -24,7 +24,7 @@ export default function LogInPage() {
           </div>
           <div className="mt-6 text-center">
             <span className="text-xs text-gray-500">
-              Copyright 2025 May Sunktong
+              © 2025 May Sunktong. All rights reserved.
             </span>
           </div>
         </div>
