e976c975be19ed4295396495a01ca0b3d7e8f519
May Sunktong <pattaranan.sunktong@gmail.com>
2025-10-14 20:47:26 +0200

    add await to checkLoginCookie

--- utils/getLogInCookies.ts
+++ utils/getLogInCookies.ts
@@ -3,7 +3,7 @@
 import { cookies } from "next/headers";
 
 export async function checkLoginCookie() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const hasCookie = cookieStore.get("login_success");
   if (hasCookie) {
     cookieStore.delete("login_success");
